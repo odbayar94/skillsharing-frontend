8bd52593120829b5b2782ea1db2113d1f25e264b
Odbayar <odbayar94@gmail.com>
2021-11-20 17:30:58 +0800

    fix assets

--- src/components/dashboard/timeline/Timeline.js
+++ src/components/dashboard/timeline/Timeline.js
@@ -7,13 +7,6 @@ import {
    
 } from 'reactstrap';
 
-import img1 from '../../../assets/images/users/1.jpg';
-import img2 from '../../../assets/images/big/img1.jpg';
-import img3 from '../../../assets/images/big/img2.jpg';
-import img4 from '../../../assets/images/big/img3.jpg';
-import img5 from '../../../assets/images/big/img5.jpg';
-import img6 from '../../../assets/images/users/2.jpg';
-
 import img8 from '../../../assets/images/users/4.jpg';
 
 const TimeLine = ({date, title, context, comments, claps,id}) => {
